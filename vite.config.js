570102636904bff03c55e32ef7a1b05ff14dc9ac
christian <christianalonso9702@gmail.com>
2026-06-25 00:38:05 -0500

    se modifico el tamaño de la tabla y se posiciono en el centro de la pagina

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://christianalonso.github.io/rnewspa",
-  build:{
-    rollupOptions:{
-      input:{
-        main: "./index.html",
-        template:"./template.html"
-      }
-    }
-  }
+  base:"https://christianalonso.github.io/rnewspa"
 })
